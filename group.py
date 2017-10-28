bebf329b033fa0c96f0c68806a5caad90a853ec6
zbikowa <agnieszka.jasiu@gmail.com>
2017-10-28 23:02:39 +0200

    changes in group object

--- group.py
+++ group.py
@@ -4,3 +4,17 @@ class Group:
         self.name = name
         self.header = header
         self.footer = footer
+
+    def __init__(self, name, middlename, lastname, nickname, title, company, address, home, mobile, email,
+                       byear):
+        self.name = name
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.email = email
+        self.byear = byear
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
 import unittest
+from group import Group
 
 def is_alert_present(wd):
     try:
@@ -19,69 +19,69 @@ class test_add_contact(unittest.TestCase):
     def test_test_add_contact(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/group.php")
-        self.login(wd)
-        self.create_contact(wd)
+        self.login(wd, username="admin", password="secret")
+        self.create_contact(wd, Group(name="Jan", middlename="Adam", lastname="Nowak", nickname="jnow", title="mr", company="Intel", address="Starowiejska 10/4", home="400340340", mobile="\\9",
+                            email="jan.nowak@gmail.com", byear="1987"))
         self.logout(wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def create_contact(self, wd, name="Jan", middlename="Adam", lastname="Nowak", nickname="jnow", title="mr", company="Intel",
-                       address="Starowiejska 10/4", home="400340340", mobile="\\9", email="jan.nowak@gmail.com", byear="1987"):
+    def create_contact(self, wd, group):
         wd.find_element_by_link_text("add new").click()
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(name)
+        wd.find_element_by_name("firstname").send_keys(group.name)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(middlename)
+        wd.find_element_by_name("middlename").send_keys(group.middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(group.lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(nickname)
+        wd.find_element_by_name("nickname").send_keys(group.nickname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(title)
+        wd.find_element_by_name("title").send_keys(group.title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(company)
+        wd.find_element_by_name("company").send_keys(group.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(address)
+        wd.find_element_by_name("address").send_keys(group.address)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(home)
+        wd.find_element_by_name("home").send_keys(group.home)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(address)
+        wd.find_element_by_name("address").send_keys(group.address)
         wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").send_keys(mobile)
+        wd.find_element_by_name("mobile").send_keys(group.mobile)
         wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").send_keys(mobile)
+        wd.find_element_by_name("work").send_keys(group.mobile)
         wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").send_keys(mobile)
+        wd.find_element_by_name("fax").send_keys(group.mobile)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(email)
+        wd.find_element_by_name("email").send_keys(group.email)
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[23]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[23]").click()
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[3]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[3]").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(byear)
+        wd.find_element_by_name("byear").send_keys(group.byear)
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def tearDown(self):
