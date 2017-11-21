7e13a1b7bb61400d41ec83d619ab98be1ee4bd52
zbikowa <agnieszka.jasiu@gmail.com>
2017-11-21 21:58:53 +0100

    verification for test modify contact

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,4 +4,11 @@ from model.contact import Contact
 def test_modify_contact_name(app):
     if app.contact.count():
         app.contact.create(Contact(firstname="new contact", middlename="new"))
-    app.contact.modify_first_contact(Contact(firstname="changed name", middlename="bew"))
+    old_contact = app.contact.get_contact_list()
+    contact = Contact(firstname="changed name", middlename="bew")
+    contact.id = old_contact[0].id
+    app.contact.modify_first_contact(contact)
+    new_contact = app.contact.get_contact_list()
+    assert len(old_contact) == len(new_contact)
+    old_contact[0] = contact
+    assert sorted(old_contact, key=Contact.id_or_max) == sorted(new_contact, key=Contact.id_or_max)
