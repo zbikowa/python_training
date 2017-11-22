ede6fea90fa674109df1f0390a8546cd794267a4
zbikowa <agnieszka.jasiu@gmail.com>
2017-11-22 17:08:44 +0100

    cache added to add group test

--- fixture/group.py
+++ fixture/group.py
@@ -21,6 +21,7 @@ class GroupHelper:
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
@@ -51,6 +52,7 @@ class GroupHelper:
         wd.find_element_by_name("update").click()
         #return to groups
         self.return_to_groups_page()
+        self.group_cache = None
 
     def delete_first_group(self):
         wd = self.app.wd
@@ -60,6 +62,7 @@ class GroupHelper:
         #submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def return_to_groups_page(self):
         wd = self.app.wd
@@ -69,17 +72,19 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         try:
-            wd.find_element_by_name("selected[]")
-            return True
+            return len(wd.find_elements_by_name("selected[]"))
         except NoSuchElementException:
-            return False
+            return 0
+
+    group_cache = None
 
     def get_group_list(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        groups = []
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            groups.append(Group(name=text, id=id))
-        return groups
+        if self.group_cache is None:
+            wd = self.app.wd
+            self.open_groups_page()
+            self.group_cache = []
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name=text, id=id))
+        return list(self.group_cache)
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-from sys import maxsize
 
 
 def test_add_group(app):
@@ -8,16 +7,16 @@ def test_add_group(app):
     group = Group(name="234", header="234", footer="2344")
     app.group.create(group)
     new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
+    assert len(old_groups) + 1 == app.group.count()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+#def test_add_empty_group(app):
+#    old_groups = app.group.get_group_list()
+#    group = Group(name="", header="", footer="")
+#    app.group.create(group)
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups) + 1 == len(new_groups)
+#    old_groups.append(group)
+#    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
