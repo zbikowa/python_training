18711bbe6b08bfcf15ca4b5b42177d770ce5f41a
zbikowa <agnieszka.jasiu@gmail.com>
2017-12-03 11:17:24 +0100

    parametrisation for add contact test (address email and phone numbers

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -10,23 +10,25 @@ def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
     return prefix + "".join([random.choice(symbols)for i in range(random.randrange(maxlen))])
 
-#
-# testdata = [
-#     Contact(firstname=firstname, lastname=lastname, address=address, email=email)
-#     # for firstname in ["", random_string("firstname", 10)]
-#     # for lastname in ["", random_string("lastname", 20)]
-#     # for address in ["", random_string("address", 20)]
-#     # for email in ["", random_string("email", 20)]
-# ]
+
+def random_number(prefix, maxlen):
+    numbers = string.digits*10
+    return prefix + "".join([random.choice(numbers) for i in range(random.randrange(maxlen))])
+
+
 testdata = [
-    Contact(firstname="", lastname="", address="")] + [
-    Contact(firstname=random_string("firstname", 8), lastname=random_string("lastname", 12), address=random_string("address", 12))
-    for i in range(5)
-]
+    Contact(firstname="", lastname="", address="", email="")] + [
+    Contact(firstname=random_string("firstname", 8), lastname=random_string("lastname", 12),
+            address=random_string("address", 12), email="agnieszka@agnieszka.pl",
+            homephone=random_number("+", 9), mobilephone=random_number("+", 9), workphone=random_number("+", 9),
+            secondaryphone=random_number("+", 9))
+    for i in range(10)
+            ]
+
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
 def test_add_contact(app, contact):
-    pass
+    #pass
     old_contact = app.contact.get_contact_list()
     app.contact.create(contact)
     new_contact = app.contact.get_contact_list()
