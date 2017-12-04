6caf2439238475a1647521cbf8f4cc11f7dfd710
zbikowa <agnieszka.jasiu@gmail.com>
2017-12-04 21:32:27 +0100

    config current directory changes

--- conftest.py
+++ conftest.py
@@ -1,20 +1,25 @@
 # -*- coding: utf-8 -*-
 import pytest
+import json
+import os.path
 from fixture.application import Application
 
 fixture = None
+target = None
+
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_Url = request.config.getoption("--baseUrl")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_Url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_Url)
-    fixture.session.ensure_login(username="admin", password="secret")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+        if fixture is None or not fixture.is_valid():
+            fixture = Application(browser=browser, base_url=target['baseUrl'])
+        fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -29,4 +34,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/group.php")
+    parser.addoption("--target", action="store", default="target.json")
