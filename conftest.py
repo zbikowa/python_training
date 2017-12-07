ea92eefd1f1dbe9a08078ee93056e577e75eb46f
zbikowa <agnieszka.jasiu@gmail.com>
2017-12-07 15:39:18 +0100

    error

--- conftest.py
+++ conftest.py
@@ -54,5 +54,5 @@ def load_from_module(module):
 
 
 def load_from_json(file):
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/test.json")) as f:
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
         return jsonpickle.decode(f.read())
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,37 +1,25 @@
 #-*- coding: utf-8 -*-
 from model.contact import Contact
-from sys import maxsize
-import pytest
 import random
 import string
 
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols)for i in range(random.randrange(maxlen))])
-
-
-def random_number(prefix, maxlen):
-    numbers = string.digits*10
-    return prefix + "".join([random.choice(numbers) for i in range(random.randrange(maxlen))])
-
-
-testdata = [
-    Contact(firstname="", lastname="", address="", email="")] + [
-    Contact(firstname=random_string("firstname", 8), lastname=random_string("lastname", 12),
-            address=random_string("address", 12), email="agnieszka@agnieszka.pl",
-            homephone=random_number("+", 9), mobilephone=random_number("+", 9), workphone=random_number("+", 9),
-            secondaryphone=random_number("+", 9))
-    for i in range(10)
-            ]
+#
+# def random_string(prefix, maxlen):
+#     symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+#     return prefix + "".join([random.choice(symbols)for i in range(random.randrange(maxlen))])
+#
+#
+# def random_number(prefix, maxlen):
+#     numbers = string.digits*10
+#     return prefix + "".join([random.choice(numbers) for i in range(random.randrange(maxlen))])
 
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
-    #pass
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contact = app.contact.get_contact_list()
     app.contact.create(contact)
+    #assert len(old_contact) + 1 == len(new_contact)
+    assert len(old_contact) + 1 == app.contact.count()
     new_contact = app.contact.get_contact_list()
-    assert len(old_contact) + 1 == len(new_contact)
     old_contact.append(contact)
     assert sorted(old_contact, key=Contact.id_or_max) == sorted(new_contact, key=Contact.id_or_max)
