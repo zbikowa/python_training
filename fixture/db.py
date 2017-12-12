57686c90de0bed2a0143392b6d07e4504ba8921c
zbikowa <agnieszka.jasiu@gmail.com>
2017-12-12 23:12:37 +0100

    add group refactored

--- fixture/db.py
+++ fixture/db.py
@@ -1,16 +1,17 @@
 import pymysql.cursors
 from model.group import Group
 
+
 class DbFixture():
     def __init__(self, host, name, user, password):
         self.host = host
         self.name = name
         self.user = user
         self.password = password
-        self.connection = pymysql.connect(host=host, database=name, user=user, password=password)
+        self.connection = pymysql.connect(host=host, database=name, user=user, password=password, autocommit=True)
 
     def get_group_list(self):
-        list = []
+        list =[]
         cursor = self.connection.cursor()
         try:
             cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
