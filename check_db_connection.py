ca68de87a3bd52b028d6130e4991101671296ac5
zbikowa <agnieszka.jasiu@gmail.com>
2017-12-13 17:33:52 +0100

    orm for groups and contacts

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,11 +1,11 @@
-import pymysql.cursors
+from fixture.orm import ORMFixture
 
-connection = pymysql.connect(host="localhost", database="addressbook", user="root", password="")
+db = ORMFixture(host="localhost", name="addressbook", user="root", password="")
 
 try:
-    cursor = connection.cursor()
-    cursor.execute("select * from group_list")
-    for row in cursor.fetchall():
-        print(row)
+   l = db.get_contact_list()
+   for item in l:
+    print(item)
+    print(len(l))
 finally:
-    connection.close()
+    pass#db.destroy()
