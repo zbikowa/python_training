9933e75aad50f3e5f012bf83c93d861c4414ece6
zbikowa <agnieszka.jasiu@gmail.com>
2017-12-18 16:24:11 +0100

    test add contact from db

---
+++ data/contacts.py
@@ -0,0 +1,7 @@
+from model.contact import Contact
+
+testdata = \
+    [
+    Contact(firstname="Firstname1", lastname="Lastname1"),
+    Contact(firstname="Firstname2", lastname="Lastname2")
+]
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,25 +1,11 @@
 #-*- coding: utf-8 -*-
 from model.contact import Contact
-import random
-import string
-
-#
-# def random_string(prefix, maxlen):
-#     symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-#     return prefix + "".join([random.choice(symbols)for i in range(random.randrange(maxlen))])
-#
-#
-# def random_number(prefix, maxlen):
-#     numbers = string.digits*10
-#     return prefix + "".join([random.choice(numbers) for i in range(random.randrange(maxlen))])
 
 
-def test_add_contact(app, json_contact):
-    fixture_contact = json_contact
-    old_contacts = app.contact.get_contact_list()
+def test_add_contact(app, db, data_contacts):
+    fixture_contact = data_contacts
+    old_contacts = db.get_contact_list()
     app.contact.create(fixture_contact)
-    #assert len(old_contact) + 1 == len(new_contact)
-    assert len(old_contacts) + 1 == app.contact.count()
-    new_contact = app.contact.get_contact_list()
+    new_contact = db.get_contact_list()
     old_contacts.append(fixture_contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contact, key=Contact.id_or_max)
