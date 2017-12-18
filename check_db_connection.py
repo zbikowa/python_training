31f069d856470d925ee4219682ec56bc7b24b02e
zbikowa <agnieszka.jasiu@gmail.com>
2017-12-18 16:11:04 +0100

    orm for groups and contacts

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,11 +1,15 @@
 from fixture.orm import ORMFixture
+from model.group import Group
+
 
 db = ORMFixture(host="localhost", name="addressbook", user="root", password="")
 
+
+
 try:
-   l = db.get_contact_list()
-   for item in l:
-    print(item)
+    l = db.get_contacts_in_group(Group(id="11"))
+    for item in l:
+        print(item)
     print(len(l))
 finally:
-    pass#db.destroy()
+    pass # db.destroy()
